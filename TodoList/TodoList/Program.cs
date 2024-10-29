65be34f7dda736e4c5d5e298ec63105bff2cfdf1
Tyler Payne <154254574+Yhento@users.noreply.github.com>
2024-10-29 11:23:14 -0500

    first commit

---
+++ TodoList/TodoList/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("[S]ee all TODOS");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+string userChoice = Console.ReadLine();
+System.Console.WriteLine(userChoice);
+Console.WriteLine("User input: " + userChoice);
+userChoice = "ABC";
+
+var result = "abc" + "def" + "ghi";
+
+Console.WriteLine(result);
+
+
