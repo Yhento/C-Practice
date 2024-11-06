5adb0c6c0793c0a33e4a638237101d269b4c49f3
Yhento <154254574+Yhento@users.noreply.github.com>
2024-11-05 19:38:24 -0600

    finish Assignment.

--- My_TodoList/Program.cs
+++ My_TodoList/Program.cs
@@ -1,167 +1,112 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Transactions;
 using Microsoft.VisualBasic;
+var todos = new List<string>();
+bool shallExit = false;
+Console.WriteLine("Hello, World!");
 
-List<string> valid_inputs = new List<string> { "S", "A", "R", "E" };
-List<string> Todos = new List<string>();
-bool isValidInput;
-string userInput;
-do
-{
-    Console.WriteLine("Hello, World!");
-    Console.WriteLine("What would you like to do?");
-    PrintOptions();
-
-
-    do
-    {
-        userInput = Console.ReadLine().ToUpper();
-
-        isValidInput = valid_inputs.Contains(userInput);
-
-        if (!isValidInput)
-        {
-            Console.WriteLine("Incorrect Input!");
-            PrintOptions();
-        }
-
-    } while (!isValidInput);
-
-    switch (userInput)
-    {
-        case "S":
-            if (!Todos.Any())
-            {
-                Console.WriteLine("No TODOs have been added yet.");
-            }
-            else
-            {
-                printTodos(Todos);
-            }
-            break;
-
-        case "A":
-            bool todoIsDuplicate;
-            bool todoIsEmpty;
-            do
-            {
-                Console.WriteLine("Enter the TODO description: ");
-                string todo_to_add = Console.ReadLine();
-                todoIsDuplicate = IsTodoDuplicate(todo_to_add);
-                todoIsEmpty = IsEmpty(todo_to_add);
-                if (todoIsEmpty)
-                {
-                    System.Console.WriteLine("The description cannot be empty");
-                }
-                else if (todoIsDuplicate)
-                {
-                    System.Console.WriteLine("The description must be unique");
-                }
-                else
-                {
-                    AddTodo(todo_to_add);
-                }
-            } while (todoIsDuplicate || todoIsEmpty);
-            break;
-        case "R":
-            bool IndexIsNumber;
-            bool IndexIsEmpty;
-            bool IndexIsValid;
-            string removed_item;
-         
-            if (!Todos.Any())
-            {
-                Console.WriteLine("No TODOs have been added yet.");
-                break;
-            }
-
-            do
-            {
-                System.Console.WriteLine("Select the index of the Todo you want to remove:");
-                printTodos(Todos);
-                string index_to_remove = Console.ReadLine();
-                IndexIsEmpty = IsEmpty(index_to_remove);
-                IndexIsNumber = int.TryParse(index_to_remove, out int index);
-                
-                if (IndexIsEmpty){
-                    System.Console.WriteLine("Selected Index Cannot Be Empty.5");
-                    IndexIsValid = false;
-                }
-                else if (!IndexIsNumber || !IsIndexInList(Todos, index - 1))
-                {
-                    System.Console.WriteLine("The given index is not valid");
-                    IndexIsValid = false;
-                }
-                else
-                {
-                    IndexIsValid = true;
-                    removed_item = Todos[index-1];
-                    RemoveTodo(Todos,index-1);
-                    System.Console.WriteLine($"TODO Removed: {removed_item}");
-                }
-
-
-            } while (!IndexIsValid);
-            break;
-    }
-
-
-
-} while (userInput.ToUpper() != "E");
-
-
-void PrintOptions()
+while (!shallExit)
 {
     Console.WriteLine("[S]ee all TODOS");
     Console.WriteLine("[A]dd a TODO");
     Console.WriteLine("[R]emove a TODO");
     Console.WriteLine("[E]xit");
-}
+    string userChoice = Console.ReadLine();
 
-void printTodos(List<string> Todos)
-{
-    for (int i = 0; i < Todos.Count(); i++)
+    switch (userChoice)
     {
-        Console.WriteLine($"{i + 1}. {Todos[i]}");
+        case "s":
+        case "S":
+            PrintTodos();
+            break;
+        case "a":
+        case "A":
+            AddTodo();
+            break;
+        case "r":
+        case "R":
+            RemoveTodo();
+            break;
+        case "e":
+        case "E":
+            shallExit = true;
+            break;
+        default:
+            Console.WriteLine("Invalid Choice");
+            break;
     }
 }
 
-void AddTodo(string userInput)
-{
-    Todos.Add(userInput);
-    System.Console.WriteLine($"TODO successfully added: {userInput}");
-}
 
-void RemoveTodo(List<string> Todos, int index)
+void PrintTodos()
 {
-    Todos.RemoveAt(index);
-}
-
-bool IsTodoDuplicate(string todo)
-{
-    if (Todos.Contains(todo))
-    {
-        return true;
-    }
-    return false;
-}
-
-bool IsEmpty(string userInput)
-{
-    if (string.IsNullOrWhiteSpace(userInput))
-    {
-        return true;
-    }
-    return false;
-}
-
-bool IsIndexInList(List<string> todos, int index)
-{
-    if (todos != null && index >= 0 && index < todos.Count)
-    {
-        return true;
+    if (todos.Count == 0) {
+        Console.WriteLine("No todos have been added yet");
     }
     else
     {
-        return false;
+        for (int i=0; i < todos.Count; i++)
+        {
+            System.Console.WriteLine($"{i+1}: {todos[i]}");
+        }
     }
+}
+
+void AddTodo()
+{
+    bool isValid = false;
+    while (!isValid)
+    {
+        Console.WriteLine("Enter a description:");
+        var description = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(description))
+        {
+            Console.WriteLine("Description cannot be empty");
+        }
+        else if (todos.Contains(description))
+        {
+            System.Console.WriteLine("Description cannot be a duplicate!");
+        }
+        else
+        {
+            todos.Add(description);
+            isValid = true;
+        }
+    }
+
+}
+
+void RemoveTodo() {
+   if (!todos.Any()) 
+   {
+        Console.WriteLine("No TODOs have been added yet");
+   }
+   else
+   {
+        bool isValid = false;
+        while(!isValid)
+        {
+            PrintTodos();
+            Console.WriteLine("Select the Index of the Todo to remove:");
+            var selected_index = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(selected_index))
+            {
+                Console.WriteLine("Index cannot be empty!");
+            }
+            else if (!int.TryParse(selected_index, out int index) || index < 1 || index > todos.Count())
+            {
+                Console.WriteLine("The given index is not valid.");
+            }
+            else
+            {
+                var removed_item = todos[index-1];
+                todos.Remove(removed_item);
+                Console.WriteLine($"TODO Removed: {removed_item}");
+                isValid = true;
+            }
+        }
+   }
 }
